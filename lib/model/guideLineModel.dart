d11f3ac0a46076209bd70562b0f09464740413e0
Ricky-karunakaran <86287753+Ricky-karunakaran@users.noreply.github.com>
2022-06-14 23:53:31 +0800

    Create guidelineModel

--- lib/model/guideLineModel.dart
+++ lib/model/guideLineModel.dart
@@ -1,14 +1,17 @@
+import 'package:helbage/model/tagModel.dart';
+
 class guidelineModel{
   String title;
   String content;
   String author;
+  List<tagModel> tag;
   DateTime? datetime;
   //constructor for created new guideline
-  guidelineModel({required this.title,required this.content,required this.author})
+  guidelineModel({required this.title,required this.content,required this.author,required this.tag})
   {
     this.datetime=DateTime.now();
   }
-  
+
   Map<String, dynamic> toFirestore() {
     return {
       "title": title,
