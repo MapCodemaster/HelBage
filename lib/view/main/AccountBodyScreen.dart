bc9e6b72d3dcc1e8570d3bbeeaf081f78696d23d
WilliamHoZeQian <103400938+WilliamHoZeQian@users.noreply.github.com>
2022-06-27 18:19:32 +0800

    Add files via upload

--- lib/view/main/AccountBodyScreen.dart
+++ lib/view/main/AccountBodyScreen.dart
@@ -47,7 +47,40 @@ class _AccountBodyScreenState extends State<AccountBodyScreen> {
                             Icons.arrow_forward_ios,
                             size: 16,
                             color: Colors.black,
-                          )
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    model.NavigateToVehicleView();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                    child: SafeArea(
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.edit,
+                            size: 20,
+                            color: primaryColor,
+                          ),
+                          SizedBox(width: 20),
+                          Text(
+                            "Vehicle Detail",
+                            style: TextStyle(
+                              fontSize: 16, //16
+                              color: Colors.black,
+                            ),
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                            color: Colors.black,
+                          ),
                         ],
                       ),
                     ),
@@ -65,7 +98,8 @@ class _AccountBodyScreenState extends State<AccountBodyScreen> {
                     TextStyle(
                       color: Colors.white,
                       fontSize: 20,
-                    ))
+                    ),
+                    ),
               ],
             ),
           );
